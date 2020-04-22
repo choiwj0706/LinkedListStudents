65527bb8a45acdf144d13f53791da4f788afa848
choiwj <choiwj0706@naver.com>
2020-04-22 21:17:15 +0900

    input println("*         *")

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -15,6 +15,7 @@ public class LinkedList {
         }else{
             Node n = head;
             System.out.println("학생 리스트");
+            System.out.println("*          *");
             System.out.println("************");
             while(n!=null){
                 //JOptionPane.showMessageDialog(null, "See the console for students list.", "Linked List", JOptionPane.INFORMATION_MESSAGE);
